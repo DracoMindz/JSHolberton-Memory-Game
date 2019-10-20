a8a50fe5c8c65eb86c24432a0bf8a253c6f4e898
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2019-10-19 21:29:49 -0400

    Update

--- core/game.js
+++ core/game.js
@@ -180,16 +180,3 @@ function resetGame() {
         newGame(); // Calls resetBoard() aswell, no need to do it in this function
     }, 900);
 }
-
-function startTimer() {
-    gameStarted = true
-
-    var sec = 0;
-
-    function pad(val) { return val > 9 ? val : "0" + val; }
-    setInterval(function() {
-        document.getElementById("seconds").innerHTML = pad(++sec % 60);
-        document.getElementById("minutes").innerHTML = pad(parseInt(sec / 60, 10));
-    }, 1000); <
-    span id = "minutes" > < /span>:<span id="seconds"></span >
-}
