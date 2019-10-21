0d8c50ec82e4a32a079aa296c6ee1efa37209ec1
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2019-10-21 06:06:33 -0400

    UPdate timer

--- core/gametimer.js
+++ core/gametimer.js
@@ -2,16 +2,16 @@
 var minutesLabel = document.getElementById("minutes");
 var secondsLabel = document.getElementById("seconds");
 var totalSeconds = 0;
-gameStarted = true
-setInterval(setTime, 1000);
+
+// New game functionallity
+const card = document.getElementsByClassName('card')[0];
+card.addEventListener('click', () => {
+    setInterval(setTime, 1000);
+});
+//gameStarted = true
+// setInterval(setTime, 1000);
+
 //set minutes 
-
-function setTime() {
-    ++totalSeconds;
-    secondsLabel.innerHTML = pad(totalSeconds % 60);
-    minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
-}
-
 function pad(val) {
     var valString = val + "";
     if (valString.length < 2) {
@@ -26,6 +26,14 @@ function set_timer() {
     secondsLabel = document.getElementById("seconds");
     my_int = setInterval(function() { setTime(minutesLabel, secondsLabel) }, 1000);
 }
+
+function setTime() {
+    ++totalSeconds;
+    secondsLabel.innerHTML = pad(totalSeconds % 60);
+    minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+}
+
+
 gameStarted = false;
 
 function stop_timer() {
